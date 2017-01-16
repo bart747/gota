ff40b7fe87d641c1a20fbaadc0f2007c96321552
Bartosz Wieczorek <hello.bartosz@gmail.com>
2017-01-16 02:47:02 +0100

    reorganize file structure move test app to separate project folder

--- gota.go
+++ gota.go
@@ -1,24 +1,112 @@
-package main
+package gota
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/bart747/gota/builders"
+	"os"
+	"path"
 )
 
+func check(firstMessage string, e error) {
+	if e != nil {
+		log.Println(firstMessage, ": ", e)
+		return
+	}
+}
+
+/*
+func extendPaths(paths []string, ext string) []string {
+	vsm := make([]string, len(paths))
+	for i, v := range paths {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+*/
+func extendPaths(ext string, paths []string) []string {
+	newCol := make([]string, len(paths))
+	for i, v := range paths {
+		newCol[i] = ext + "/" + v
+	}
+	return newCol
+}
+
+// create a new page from premade templates
+func CreatePage(filepath string, templates []string) {
+	tmplPaths := extendPaths("templates", templates)
+	tmpl, err := template.ParseFiles(tmplPaths...)
+	check("parse template files", err)
+
+	file, err := os.Create(filepath)
+	check("create file", err)
+
+	err = tmpl.ExecuteTemplate(file, "layout", nil)
+	check("execute template", err)
+
+	file.Close()
+	file.Sync() // flush in-memory copy
+	log.Println(filepath + " created")
+
+	return
+}
+
+// serve custom page made from premade templates
+func ServeCustomPage(subpath, baseTmpl string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		layoutT := path.Join("templates", baseTmpl)
+		contentT := path.Join("templates", r.URL.Path[len(subpath):])
+
+		// return a 404 if the template doesn't exist
+		fileInfo, err := os.Stat(contentT)
+		if err != nil {
+			if os.IsNotExist(err) {
+				http.NotFound(w, r)
+				return
+			}
+		}
+		// return a 404 if the request is for a directory
+		if fileInfo.IsDir() {
+			http.NotFound(w, r)
+			return
+		}
+
+		tmpl, err := template.ParseFiles(layoutT, contentT)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		err = tmpl.ExecuteTemplate(w, "layout", nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+	}
+}
+
+/*
+** Go Template Assembler **
+    * EXAMPLE of use *
+
 func main() {
-	tmplstr := []string{"templates/layout.html", "templates/example.html"}
-	builders.CreatePage("public/testpage.html", tmplstr...)
+	// create pages by composing templates together with slices:
+	templates := []string{"templates/layout.html", "templates/content.html"}
+	gota.CreatePage("public/testpage.html", templates...)
 
 	fs := http.FileServer(http.Dir("public"))
-
 	http.Handle("/", http.StripPrefix("/", fs))
 
-	http.HandleFunc("/custom/", builders.ServeCustomPage("/custom/"))
-	log.Println(":9090", "Listening...")
-	err := http.ListenAndServe(":9090", nil)
+	// you can quickly serve custom pages by locahost:3000/custom/pickSomeTemplate.html
+	http.HandleFunc("/custom/", builders.ServeCustomPage("/custom/", "layout.html"))
+
+	log.Println(":3000", "Listening...")
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Print("server: ", err)
 	}
 }
+*/
